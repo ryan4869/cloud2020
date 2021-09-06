f1f0fc9a10b8e1200b25b45ed060cae191b9b815
ryan <7410200+ryangjun@user.noreply.gitee.com>
2021-09-06 23:28:59 +0800

    eureka

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -3,9 +3,17 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.XSlf4j;
+import org.slf4j.ILoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+import sun.rmi.runtime.Log;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class PaymentController {
@@ -14,6 +22,12 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -38,4 +52,20 @@ public class PaymentController {
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element: services) {
+            System.out.println("***element"+element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances(applicationName);
+        for (ServiceInstance instance:instances) {
+            System.out.println("***instanceId"+instance.getInstanceId()+"***host"+instance.getHost());
+
+        }
+        return this.discoveryClient;
+    }
+
 }
